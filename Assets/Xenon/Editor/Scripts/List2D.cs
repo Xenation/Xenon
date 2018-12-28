75746fcb8e679bf02a3d51033544a238601ae6c4
Xenation <xenations.111@gmail.com>
2018-12-28 15:57:04 +0100

    Old Uncommited Things

---
+++ Assets/Xenon/Editor/Scripts/List2D.cs
@@ -0,0 +1,22 @@
+﻿namespace Xenon.Editor {
+	public class List2D<T> {
+
+		private T[,] grid;
+		
+		public T this[int x, int y] {
+			get {
+				return grid[x, y];
+			}
+			set {
+				grid[x, y] = value;
+			}
+		}
+
+		public List2D(int sizeX, int sizeY) {
+			grid = new T[sizeX, sizeY];
+		}
+
+		public List2D() : this(1, 1) { }
+
+	}
+}
