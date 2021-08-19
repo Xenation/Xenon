57b99164fc79a83f4c89db9b9acc4ff685ab981c
Xenation <xenations.111@gmail.com>
2021-08-19 22:12:43 +0200

    Implemented Screenshot Tool Resolution Supersizing

--- Packages/dev.halfred.xenon.screenshot/Editor/ScreenshotTool.cs
+++ Packages/dev.halfred.xenon.screenshot/Editor/ScreenshotTool.cs
@@ -49,47 +49,58 @@ namespace Xenon.Screenshot {
 				Debug.Log(filepath);
 
 				Camera camera = targetCamera;
+				bool isSceneView = false;
 				if (useSceneCamera) {
 					camera = SceneView.lastActiveSceneView.camera;
+					isSceneView = true;
 				} else if (camera == null) {
 					Debug.Log("No Target Camera given! Falling back to main camera or any available.");
 					camera = FindCamera();
 					if (camera == null) {
 						Debug.Log("No available camera found! Falling back to scene view camera.");
 						camera = SceneView.lastActiveSceneView.camera;
+						isSceneView = true;
 					}
 				}
+
+				camera = CopyCamera(camera, isSceneView);
 
 				if (camera != null) {
 					TakeScreenshotFromCamera(camera, filepath);
 				}
+
+				DestroyImmediate(camera.gameObject);
 			}
 		}
 
 		private void TakeScreenshotFromCamera(Camera camera, string filepath) {
-			RenderTexture tmpTex = camera.targetTexture;
-
-			// Create the render texture
+			// Create the render texture used for each tile
 			//RenderTextureDescriptor texDesc = new RenderTextureDescriptor(resolution.x, resolution.y, GraphicsFormat.R8G8B8_UNorm, 0, 0);
 			RenderTextureDescriptor texDesc = new RenderTextureDescriptor(resolution.x, resolution.y, RenderTextureFormat.ARGB32, 0, 0);
 			RenderTexture texture = RenderTexture.GetTemporary(texDesc);
 
+			// Create the final texture
+			Texture2D finalTexture = new Texture2D(texDesc.width * superSize, texDesc.height * superSize, TextureFormat.RGB24, false);
+
 			// Render from the camera
 			camera.targetTexture = texture;
-			camera.Render();
-
-			// Copy to a classic texture
-			Texture2D finalTexture = new Texture2D(texDesc.width, texDesc.height, TextureFormat.RGB24, false);
 			RenderTexture.active = texture;
-			finalTexture.ReadPixels(new Rect(Vector2.zero, resolution), 0, 0, false);
+			//camera.aspect = texDesc.width / texDesc.height;
+			for (int y = 0; y < superSize; y++) {
+				for (int x = 0; x < superSize; x++) {
+					camera.projectionMatrix = ComputeTileProjectionMatrix(new Vector2Int(x, y), Vector2Int.one * superSize, camera.fieldOfView * Mathf.Deg2Rad, camera.nearClipPlane, camera.farClipPlane, camera.aspect);
+					camera.Render();
+					// Copy to the final texture
+					finalTexture.ReadPixels(new Rect(Vector2.zero, resolution), texDesc.width * x, texDesc.height * y, false);
+					finalTexture.Apply();
+				}
+			}
 			RenderTexture.active = null;
 			RenderTexture.ReleaseTemporary(texture);
 
 			// Write file
 			byte[] pngBytes = finalTexture.EncodeToPNG();
 			File.WriteAllBytes(filepath, pngBytes);
-
-			camera.targetTexture = tmpTex;
 		}
 
 		private Camera FindCamera() {
@@ -111,6 +122,25 @@ namespace Xenon.Screenshot {
 			} else {
 				return cameras[0];
 			}
+		}
+
+		private Camera CopyCamera(Camera camera, bool isSceneView) {
+			GameObject tmpCameraObj = GameObject.Instantiate(camera.gameObject);
+			Camera tmpCamera = tmpCameraObj.GetComponent<Camera>();
+			if (isSceneView) {
+				tmpCamera.worldToCameraMatrix = camera.worldToCameraMatrix;
+			}
+			return tmpCamera;
+		}
+
+		private Matrix4x4 ComputeTileProjectionMatrix(Vector2Int tilePos, Vector2Int gridSize, float fov, float near, float far, float aspect) {
+			float top = Mathf.Tan(fov * 0.50f) * near;
+			float right = top * aspect;
+
+			float shiftX = right * 2 / gridSize.x;
+			float shiftY = top * 2 / gridSize.y;
+
+			return Matrix4x4.Frustum(-right + shiftX * tilePos.x, -right + shiftX * (tilePos.x + 1), -top + shiftY * tilePos.y, -top + shiftY * (tilePos.y + 1), near, far);
 		}
 
 	}
