4e6d51a0ff734c08855d0e3eef94ed6a3cd01df0
Xenation <xenations.111@gmail.com>
2022-09-14 21:53:43 +0200

    Project Settings and Test Scripts

--- Assets/Test/Scripts/TestLogger.cs
+++ Assets/Test/Scripts/TestLogger.cs
@@ -3,15 +3,16 @@
 namespace Xenon.Test {
 	public class TestLogger : MonoBehaviour {
 
-		private static ulong __marker = Log.Marker("TEST");
+		//private static ulong __marker = Log.Marker("TEST");
 
-		static TestLogger() {
-			Log.Marker("TEST");
-		}
+		//static TestLogger() {
+		//	Log.Marker("TEST");
+		//}
 
 		private void Awake() {
-			Log.Marker("TEST_METHOD");
-			Log.Info("A log with auto categories");
+			//Log.Marker("TEST_METHOD");
+			//Log.Info("A log with auto categories");
+			Log.Info("A log with no categories");
 			//Log.Info(Log.Cat("TEST"), "An awake log yay!");
 			//Log.Warn(Log.Cat("TEST", "MORE_TEST"), "A warning!");
 			//Log.Error(Log.Cat("TEST"), "ERROR");
